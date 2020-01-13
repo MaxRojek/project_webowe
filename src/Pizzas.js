2553707f1d9b9efc095f167d5bd5e8e98a5a48e2
Marcin Rojek <marcin.rojek@outlook.com>
2020-01-13 21:55:17 +0100

    fixed ordering table

--- src/Pizzas.js
+++ src/Pizzas.js
@@ -165,24 +165,46 @@ class Table extends React.Component {
 }
 
 function Pizzas() {
-  const { loading, error, data } = useQuery(PIZZAS);
+  function saveInfo() {
+    alert("Kliknięto w link.");
+  }
+
+  const { loading, error, data } = useQuery(stoliki);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>There's an error: {error.message}</p>;
   console.log(data);
   console.dir(data);
-  const dish = data.meals.map(meals => <p>{meals.name}</p>);
+  const table_id = data.Tables.map(Tables => <p>{Tables.id}</p>);
+  const table_outdoor = data.Tables.map(Tables => <p>{Tables.outdoor.toString()}</p>);
+  const table_size = data.Tables.map(Tables => <p>{Tables.size}</p>);
+  const table_start_time = data.Tables.map(Tables => <p>{Tables.start_time}</p>);
+  const table_end_time = data.Tables.map(Tables => <p>{Tables.end_time}</p>);
+  console.log(table_outdoor);
   /////////////////////////////////////// połączenie z bazą///////////////////////////////////////////////////////////////
-  const elements = ["one", "two", "three"];
 
   const items = [];
 
-  for (const [index, value] of elements.entries()) {
+  for (const [index, value] of table_id.entries()) {
     items.push(
       <tr>
-        <th scope="row">{index}</th>
-        <td>{value}</td>
-        <td>{dish[index]}</td>
-        <td>@twitter</td>
+        <th scope="row">{table_id[index]}</th>
+        <td>{table_size[index]}</td>
+        <td>{table_outdoor[index]}</td>
+        <td>{table_start_time[index]}</td>
+        <td>{table_end_time[index]}</td>
+        <td>
+          <a
+            href="#"
+            //onClick={saveInfo}
+            class="btn btn-secondary btn-lg active"
+            role="button"
+            aria-pressed="true"
+            data-toggle="modal"
+            data-target="#exampleModal"
+          >
+            +
+          </a>
+        </td>
       </tr>
     );
   }
@@ -246,34 +268,74 @@ function Pizzas() {
               <thead class="thead-dark">
                 <tr>
                   <th scope="col">id</th>
-                  <th scope="col">First</th>
-                  <th scope="col">Last</th>
-                  <th scope="col">Handle</th>
-                  <th scope="col">{dish[0]}</th>
+                  <th scope="col">size</th>
+                  <th scope="col">outdoor</th>
+                  <th scope="col">start_time</th>
+                  <th scope="col">end_time</th>
+                  <th scope="col">Order</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>the Bird</td>
-                  <td>@twitter</td>
+                  <th scope="row">0</th>
+                  <td>3</td>
+                  <td>true</td>
+                  <td>04:10:00</td>
+                  <td>04:40:00</td>
+                  <td>
+                    <a
+                      href="#"
+                      onClick={saveInfo}
+                      class="btn btn-secondary btn-lg active"
+                      role="button"
+                      aria-pressed="true"
+                    >
+                      +
+                    </a>
+                  </td>
                 </tr>
                 {items}
               </tbody>
             </table>
+          </div>
+        </div>
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Twój Koszyk
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="input-group">
+                  <div class="input-group-prepend" />
+                  <span class="input-group-text" id="">
+                    First and last name
+                  </span>
+                </div>
+                <input type="text" class="form-control" />
+                <input type="text" class="form-control" />
+              </div>
+            </div>
+
+            <div class="modal-footer" />
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">
+              Save changes
+            </button>
           </div>
         </div>
       </div>
